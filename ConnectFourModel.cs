c972dfa5d8ec8bc350937a13f8679dff40968a96
Judah Schwartz <JudahSchwartz@users.noreply.github.com>
2018-02-04 15:44:24 -0500

    Add files via upload
    added Files

---
+++ ConnectFourModel.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Data;
+
+namespace ClassWork
+{
+    class BoardModel
+    {
+        private bool?[,] _board = new bool?[6, 7];
+        public BoardModel()
+        {
+            
+        }
+        /// <summary>
+        /// Will check if the top piece on the stack is a winner. This method should
+        /// be called after each time DropPiece is called.
+        /// </summary>
+        /// 
+        /// <returns></returns>
+        public bool CheckWinner(int row,int col)
+        {
+            
+            if (CheckDown(row,col))
+            {
+                return true;
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                if (CheckLeft(row, col + i))
+                {
+                    return true;
+                }
+
+                if (CheckDownLeft(row - i, col + i))
+                {
+                    return true;
+                }
+
+                if (CheckUpLeft(row + i, col + i))
+                {
+                    return true;
+                }
+            }
+
+
+            return false;
+        }
+
+        public void DisplayBoard()
+        {
+            Console.WriteLine(" A  B  C  D  E  F  G");
+            for (int i = 0; i < 6; i++)
+            {
+                
+                for (int j = 0; j < 7; j++)
+                {
+                    if (_board[i, j] == null)
+                    {
+                        Console.Write("| |");
+
+                    }
+                    else if (_board[i, j] == false)
+                    {
+                        Console.Write("|O|");
+                    }
+                    else
+                    {
+                        
+                            Console.Write("|X|");
+                        
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+        private bool CheckUpLeft(int row, int col)
+        {
+            
+            if (row < 3 || col < 3 ||row>5||col>6)
+            {
+                return false;
+            }
+            bool? me = _board[row, col];
+            return _board[row - 1,col - 1] == me&& _board[row - 2, col - 2] == me
+                                                && _board[row - 3, col - 3] == me;
+        }
+        private bool CheckDown(int row, int col)
+        {
+            if (row > 2)
+            {
+                return false;
+            }
+            bool? me = _board[row, col];
+            return _board[row + 1, col] == me && _board[row + 2, col] == me && _board[row + 3, col] == me;
+        }
+
+        private bool CheckDownLeft(int row, int col)
+        {
+            if (row > 2 || col < 3 || col > 6 || row < 0)
+            {
+                return false;
+            }
+            bool? me = _board[row, col];
+            return _board[row + 1, col - 1] == me && _board[row + 2, col - 2] == me
+                && _board[row + 3, col - 3] == me;
+        }
+        private bool CheckLeft(int row, int col)
+        {
+            
+            if (col < 3||col>6)
+            {
+                return false;
+            }
+            bool? me = _board[row, col];
+
+            return _board[row ,col- 1] == me&& 
+                   _board[row, col - 2] == me&& _board[row , col- 3] == me;
+
+        }
+        
+        /// <summary>
+        /// Drops the piece into the board
+        /// </summary>
+        /// <param name="col">The column in which to drop the piece</param>
+        /// <param name="turn">which piece to drop</param>
+        /// <param name="rowPlaced">the row the piece is ultimately dropped into</param>
+        /// <returns>whether or not piece was dropped successfully</returns>
+        public bool DropPiece(int col, bool turn, out int rowPlaced)
+        {
+            if (_board[0, col]!=null)//The top slot is occupied
+            {
+                rowPlaced = -1;
+                return false;
+            }
+
+            int row = 0;
+            while (row<6&&_board[row, col] == null)//Get to the top occupied spot, or spot below
+            //the bottom row
+            {
+                row++;
+            }
+
+            row--;//to go to the unoccupied spot
+            _board[row, col] = turn;//place piece
+            rowPlaced = row;
+            return true;
+        }
+    }
+}
---
+++ MainProgram.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Humanizer;
+
+namespace ClassWork
+{
+    class MainProgram
+    {
+        private static bool _gameOver = false,turn = false;
+        private static int _turnCount = 0;
+
+        private static BoardModel bm;
+
+        static void Main(string[] args)
+        {
+            bm = new BoardModel();
+
+            while (!_gameOver&&_turnCount<42)
+            {
+                TakeTurn();
+            }
+            bm.DisplayBoard();
+
+            Console.WriteLine(_turnCount > 42 ? "The game tied!" : "Congratulations! You Won!");
+
+
+            Console.ReadLine();
+        }
+
+        private static void TakeTurn()
+        {
+            bm.DisplayBoard();
+            Console.WriteLine("In which column would you like to drop a piece?");
+            string input = Console.ReadLine().ToUpper();
+            int colChoice = -1;
+            try
+            {
+                char c = Char.Parse(input);
+                colChoice = c - 'A';
+                if (colChoice > 6 || colChoice < 0)
+                {
+                    throw new Exception();
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Input invalid");
+                return;
+            }
+
+            bool valid = bm.DropPiece(colChoice,turn, out int row);
+            if (!valid)
+            {
+                Console.WriteLine("Column is full");
+                return;
+            }
+
+            _gameOver = bm.CheckWinner(row, colChoice);
+            turn = !turn;
+            _turnCount++;
+            Console.WriteLine(_turnCount);
+        }
+    }
+}
